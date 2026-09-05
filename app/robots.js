8684acc0eda2c02dc973e1eda30c73b5556d0bdd
Vassilios Moulakakis <68458826+vmoulakakis@users.noreply.github.com>
2026-09-05 14:59:29 +0300

    Disallow all crawlers on private SocialMarket admin

--- app/robots.js
+++ app/robots.js
@@ -1,11 +1,5 @@
-const BASE = 'https://socialmarket-theta.vercel.app';
-
 export default function robots() {
   return {
-    rules: [
-      { userAgent: '*', allow: ['/affinity-b2b/', '/affinity-b2b'], disallow: ['/go/', '/api/', '/configuration', '/scheduler'] },
-    ],
-    sitemap: `${BASE}/sitemap.xml`,
-    host: BASE,
+    rules: [{ userAgent: '*', disallow: '/' }],
   };
 }
