31578734e1ef7eb8bd9ccad421d05e0862620a5f
Vassilios Moulakakis <68458826+vmoulakakis@users.noreply.github.com>
2026-09-18 23:36:41 +0300

    Open LuxeCorner public SEO routes to crawlers

--- app/robots.js
+++ app/robots.js
@@ -1,5 +1,9 @@
 export default function robots() {
   return {
-    rules: [{ userAgent: '*', disallow: '/' }],
+    rules: [
+      { userAgent: '*', allow: ['/marketplace','/marketplace/','/luxecorner','/luxecorner/'], disallow: ['/admin','/configuration','/analytics','/scheduler','/products','/merchants','/demand','/forecast-products','/niches','/creatives','/optimization','/market','/api/'] },
+    ],
+    sitemap: 'https://socialmarket-theta.vercel.app/sitemap.xml',
+    host: 'https://socialmarket-theta.vercel.app',
   };
 }
